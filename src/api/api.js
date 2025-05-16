e4e396b439f4b8677925c29afb3b80902d9ad95c
Haaju14 <phuchau22923@gmail.com>
2025-05-16 09:56:49 +0700

    vite

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.100.134:8080/",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://192.168.100.134:8080/",
 });
 
 API.interceptors.request.use((config) => {
